1ff5acf273519771d9c0345b562c69fb05376f47
Rory Siems <roryrs@gmail.com>
2018-04-18 12:35:21 -0700

    Another pass at set_firewall recipe

--- cookbooks/poc_splunk_os_prereqs/recipes/set_firewall.rb
+++ cookbooks/poc_splunk_os_prereqs/recipes/set_firewall.rb
@@ -4,9 +4,9 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-port_numbers = [8000, 8080, 8089, 9997, 9996]
+port_numbers = ['8000', '8080', '8089', '9997', '9996']
 
-execute [recipe][port_number].each do
+execute [recipe][port_numbers].each [port_number] do
   tcp_firewall_command = 'firewall-cmd --zone=public --permanent --add-port=#{port_number}/tcp'
   command = tcp_firewall_command
 end
